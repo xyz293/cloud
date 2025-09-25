39870411e995f17717c5a1e217b98fab6b519be2
倪好成功后 <2415430981@qq.com>
2025-09-25 13:01:45 +0800

    '解决重复响应'

--- src/ulits/request.js
+++ src/ulits/request.js
@@ -5,8 +5,17 @@ const request = axios.create({
     baseURL: baseURL,
     timeout: TIMEOUT
 });
+
+  const server =new Map()
 // 添加请求拦截器
 request.interceptors.request.use(function (config) {
+    const control =new AbortController()
+     config.signal = control.signal
+     if(server.get(config.url)){
+        server.get(config.url).abort()
+        server.delete(config.url)
+     }
+       server.set(config.url,control)
     // 在发送请求之前做些什么
     const token = getToken();
     if(token){
@@ -19,8 +28,8 @@ request.interceptors.request.use(function (config) {
   });
 
 // 添加响应拦截器
-request.interceptors.response.use(function (response) {
-    
+request.interceptors.response.use(function (response) {  //先是拦截器给出请求，但是呢，服务器没有给出响应，之后再次请求就会重复请求了
+     server.delete(response.config.url);
     return response;
   }, function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
